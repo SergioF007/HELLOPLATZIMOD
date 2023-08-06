a018615888fc0751d3e9bd0523ec37f6d7b02e5a
Sergio Fernández <sefernandez0711@gmail.com>
2023-08-06 15:54:12 -0500

    Inicial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SergioF007/HELLOPLATZIMOD
+
+go 1.19
---
+++ utils.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+
+// funcion publica unicia en Mayuscula
+func HelloWorlb() {
+	fmt.Println("Hello World From Platzi")
+}
